72b180508349851256273fe5f13cc8058c663526
kosaku kurino <kousaku.maron@gmail.com>
2020-08-17 18:10:45 +0900

    feat: change lesson to puzzle format

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import HelloScreen from './src/screens/HelloScreen'
+// import { NavigationContainer } from '@react-navigation/native'
+// import TabNavigator from './src/navigators/TabNavigator'
 
+// ここでリターンされるコンポーネントがアプリ上で表示される。
 const App = () => {
   return <HelloScreen />
 }
